a190bf4c34502b076bae2c768cc5e520df5a2357
Caleb Matthews <calebjmatthews@gmail.com>
2019-01-25 13:00:25 -0600

    Functional gulp watch task with test changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,41 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const historyApiFallback = require('connect-history-api-fallback');
-const watch = require('gulp-watch');
 const ts = require('gulp-typescript');
 
 var tsServerProject = ts.createProject('tsconfig.json');
 var tsClientProject = ts.createProject('tsconfig.json');
 
-gulp.task('html', (done) => {
+gulp.task('html', () => {
   return gulp.src('source/client/**/*.html')
     .pipe(gulp.dest('dist/client'));
 });
 
-gulp.task('styles', (done) => {
+gulp.task('styles', () => {
   return gulp.src('source/client/**/*.css')
     .pipe(gulp.dest('dist/client'));
 });
 
-gulp.task('scripts-server', (done) => {
+gulp.task('scripts-server', () => {
   return gulp.src('source/server/**/*.ts')
     .pipe(tsServerProject())
     .pipe(gulp.dest('dist/server'));
 })
 
-gulp.task('scripts-client', (done) => {
+gulp.task('scripts-client', () => {
   return gulp.src('source/client/**/*.ts')
     .pipe(tsClientProject())
     .pipe(gulp.dest('dist/client'));
 })
 
-gulp.task('watch', (done) => {
-  watch('client/**/*.ts', (done) => {
-    gulp.start('scripts-server');
-    browserSync.reload();
-  });
-
-  watch('client/**/*.html', (done) => {
-    gulp.start('html');
-    browserSync.reload();
-  });
-
-  watch('server/**/*.ts', (done) => {
-    gulp.start('scripts');
-    browserSync.reload();
-  });
-});
-
-gulp.task('browser-sync', (done) => {
-  browserSync.init({
-      server: {
-          baseDir: './dist',
-      },
-      middleware : [historyApiFallback()]
-  });
-  done();
-});
+function watchFiles() {
+  gulp.watch("./source/server/**/*.ts", gulp.registry().get('scripts-server'));
+  gulp.watch("./source/client/**/*.ts", gulp.registry().get('scripts-client'));
+  gulp.watch("./source/client/**/*.html", gulp.registry().get('html'));
+  gulp.watch("./source/client/**/*.css", gulp.registry().get('styles'));
+}
 
 // build task
 gulp.task('build', gulp.parallel('html', 'styles', 'scripts-server',
   'scripts-client'));
 
 // server
-gulp.task('serve', gulp.series('build', 'watch'));
+gulp.task('watch', watchFiles);
