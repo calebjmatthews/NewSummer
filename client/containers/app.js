474985fc21a1f45895b7621f0c9c0fe6d02f4bbf
Caleb Matthews <calebjmatthews@gmail.com>
2019-01-29 11:01:16 -0600

    Separate out FieldCard container

--- client/containers/app.js
+++ client/containers/app.js
@@ -2,73 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { ageSeed, harvestSeed, plantSeed } from '../actions/field';
+import FieldCard from './field_card';
 
 const TIME_STEP = (0.025 * 1000);
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      seedChoosing: false
-    }
-  }
   componentDidMount() {
     setInterval(() => {
       this.props.ageSeed(this.props.fieldState.field);
     }, TIME_STEP);
-
-    this.fieldCardClick = this.fieldCardClick.bind(this);
-    this.seedPlantClick = this.seedPlantClick.bind(this);
-  }
-
-  fieldCardClick() {
-    if (this.props.fieldState.field.seedPlanted == null) {
-      this.setState({seedChoosing: true});
-    }
-    else {
-      if (this.props.fieldState.field.seedIsMature() == true) {
-        this.props.harvestSeed(
-          this.props.fieldState.field,
-          this.props.storehouseState.storehouse
-        );
-      }
-    }
-  }
-
-  seedPlantClick(seed) {
-    this.props.plantSeed(this.props.fieldState.field, seed);
-    this.setState({seedChoosing: false});
-  }
-
-  renderFieldCardContents() {
-    if (this.state.seedChoosing == true) {
-      return (
-        <div>
-          Choose a seed:
-          {this.props.storehouseState.storehouse.seeds.getAll().map((seed) => {
-            return (
-              <div key={seed.id}
-                onClick={() => this.seedPlantClick(seed)}>
-                {seed.name}
-              </div>
-            )
-          })}
-        </div>
-      )
-    }
-    else {
-      return (
-        <div>
-          <div>
-            {this.props.fieldState.field.getSeedName()}
-          </div>
-          <div>
-            {this.props.fieldState.field.getSeedsState() + ' '}
-            {this.props.fieldState.field.getSeedsAge()}
-          </div>
-        </div>
-      )
-    }
   }
 
   render() {
@@ -77,10 +19,7 @@ class App extends Component {
         <div className="header">
           {this.props.storehouseState.storehouse.dollars + '$'}
         </div>
-  			<div className="field-card"
-          onClick={() => this.fieldCardClick()}>
-          {this.renderFieldCardContents()}
-  			</div>
+        <FieldCard />
       </div>
     )
   }
@@ -92,7 +31,7 @@ function mapStateToProps({ fieldState, storehouseState }) {
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({
-    ageSeed, harvestSeed, plantSeed
+    ageSeed
   }, dispatch)
 }
 
---
+++ client/containers/field_card.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { harvestSeed, plantSeed } from '../actions/field';
+
+class FieldCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seedChoosing: false
+    }
+  }
+  componentDidMount() {
+    this.fieldCardClick = this.fieldCardClick.bind(this);
+    this.seedPlantClick = this.seedPlantClick.bind(this);
+  }
+
+  fieldCardClick() {
+    if (this.props.fieldState.field.seedPlanted == null) {
+      this.setState({seedChoosing: true});
+    }
+    else {
+      if (this.props.fieldState.field.seedIsMature() == true) {
+        this.props.harvestSeed(
+          this.props.fieldState.field,
+          this.props.storehouseState.storehouse
+        );
+      }
+    }
+  }
+
+  seedPlantClick(seed) {
+    this.props.plantSeed(this.props.fieldState.field, seed);
+    this.setState({seedChoosing: false});
+  }
+
+  render() {
+    if (this.state.seedChoosing == true) {
+      return (
+        <div className="field-card"
+          onClick={() => this.fieldCardClick()}>
+          Choose a seed:
+          {this.props.storehouseState.storehouse.seeds.getAll().map((seed) => {
+            return (
+              <div key={seed.id}
+                onClick={() => this.seedPlantClick(seed)}>
+                {seed.name}
+              </div>
+            )
+          })}
+        </div>
+      )
+    }
+    else {
+      return (
+        <div className="field-card"
+          onClick={() => this.fieldCardClick()}>
+          <div>
+            {this.props.fieldState.field.getSeedName()}
+          </div>
+          <div>
+            {this.props.fieldState.field.getSeedsState() + ' '}
+            {this.props.fieldState.field.getSeedsAge()}
+          </div>
+        </div>
+      )
+    }
+  }
+}
+
+function mapStateToProps({ fieldState, storehouseState }) {
+  return { fieldState, storehouseState }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    harvestSeed, plantSeed
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FieldCard);
