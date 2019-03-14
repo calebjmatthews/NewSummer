702f545d961b10228f51dc1ce93b877abd415193
Caleb Matthews <calebjmatthews@gmail.com>
2019-03-14 14:22:54 -0500

    Fontawesome library integration

---
+++ client/fontawesome.js
@@ -0,0 +1,18 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faSkullCrossbones, faIceCream, faBreadSlice, faCarrot, faHollyBerry, faLemon,
+  faFireAlt, faSquare, faMortarPestle, faCompress
+} from '@fortawesome/free-solid-svg-icons';
+
+export default function() {
+  library.add(faSkullCrossbones);
+  library.add(faIceCream);
+  library.add(faBreadSlice);
+  library.add(faCarrot);
+  library.add(faHollyBerry);
+  library.add(faLemon);
+  library.add(faFireAlt);
+  library.add(faSquare);
+  library.add(faMortarPestle);
+  library.add(faCompress);
+}
--- client/index.js
+++ client/index.js
@@ -5,6 +5,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store';
 const store = configureStore();
+import configureFontawesome from './fontawesome';
+configureFontawesome();
 
 import App from './containers/app';
 import reducers from './reducers';
