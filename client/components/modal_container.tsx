bbce4ad93c05e6ed6706c589253667e14c2679db
Caleb Matthews <calebjmatthews@gmail.com>
2019-10-21 14:40:29 -0500

    Dismiss modal with click events

--- client/components/modal_container.tsx
+++ client/components/modal_container.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import { dismissModal } from '../actions/modal';
 import Modal from '../models/modal';
 import { ModalTypes } from '../models/enums/modal_types';
 
@@ -9,35 +11,60 @@ class ModalContainer extends Component {
 
   constructor(props: ModalContainerProps) {
     super(props);
+
+    this.backgroundClick = this.backgroundClick.bind(this);
+    this.okClick = this.okClick.bind(this);
+  }
+
+  backgroundClick() {
+    this.props.dismissModal();
+  }
+
+  okClick() {
+    this.props.dismissModal();
   }
 
   render() {
     let modal = this.props.modals[0];
 
-    return (
-      <div className="modal-container">
-        <div className="modal">
-          <div className="modal-header">{modal.title}</div>
-          <div className="modal-body">
-            {modal.messages.map((message, index) => {
-              return (
-                <p key={index}>{message}</p>
-              );
-            })}
+    if (modal != undefined) {
+      return (
+        <div className="modal-container">
+          <div className="modal">
+            <div className="modal-header">{modal.title}</div>
+            <div className="modal-body">
+              {modal.messages.map((message, index) => {
+                return (
+                  <p key={index}>{message}</p>
+                );
+              })}
+              <button type="button" onClick={() => this.okClick()}>Ok</button>
+            </div>
           </div>
+          <div className="modal-background" onClick={() => this.backgroundClick()}></div>
         </div>
-        <div className="modal-background"></div>
-      </div>
-    );
+      );
+    }
+    else {
+      return null;
+    }
   }
 }
 
 interface ModalContainerProps {
   modals: Modal[];
+
+  dismissModal: Function;
 }
 
 function mapStateToProps({ modals }) {
   return { modals }
 }
 
-export default connect(mapStateToProps)(ModalContainer);
+function mapDispatchToProps(dispatch: any) {
+  return bindActionCreators({
+    dismissModal
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ModalContainer);
