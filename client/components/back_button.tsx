8ec6ef8e0082f449c2bf44102b34f8ba95990821
Caleb Matthews <calebjmatthews@gmail.com>
2019-11-06 19:29:08 -0600

    Back button component to navigate one step back in card history

---
+++ client/components/back_button.tsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { revertCard } from '../actions/card';
+
+import CardState from '../models/card_state';
+
+class BackButton extends Component {
+  props: BackButtonProps;
+
+  constructor(props: BackButtonProps) {
+    super(props);
+
+    this.backButtonClick = this.backButtonClick.bind(this);
+  }
+
+  backButtonClick() {
+    this.props.revertCard(this.props.spot);
+  }
+
+  render() {
+    return (
+      <div className="back-button-wrapper">
+        <button className="back-button"
+          onClick={ () => this.backButtonClick() }>
+          <FontAwesomeIcon icon="arrow-left" />
+        </button>
+      </div>
+    );
+  }
+}
+
+interface BackButtonProps {
+  spot: number;
+
+  cardState: CardState;
+  revertCard: Function;
+}
+
+function mapStateToProps({ cardState }) {
+  return { cardState }
+}
+
+function mapDispatchToProps(dispatch: any) {
+  return bindActionCreators({ revertCard }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (BackButton);
